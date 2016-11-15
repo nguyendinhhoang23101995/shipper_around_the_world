7e7f4b74ec0732696ce095afc34e3c1c58e8d636
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-15 23:20:47 +0700

    Add scroll animation

--- ShipperAroundTheWorld/app/assets/javascripts/application.js
+++ ShipperAroundTheWorld/app/assets/javascripts/application.js
@@ -13,4 +13,25 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
+function internal_link_click (link) {
+	// Make sure this.hash has a value before overriding default behavior
+	if (link.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		var hash = link.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 900, function(){
+		
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+		});
+	} // End if
+}
