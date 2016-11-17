02ec3b748832c72c534815e2ffd4fb91b7c37517
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-17 20:28:42 +0700

    Remove old migrate files

--- ShipperAroundTheWorld/db/migrate/20161114113638_add_incivility_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIncivilityToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :incivility, :string
-  end
-end
--- ShipperAroundTheWorld/db/migrate/20161115143723_remove_incivility_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIncivilityFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :incivility, :string
-  end
-end
