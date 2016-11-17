1b9beb8298a8804ece35aa3c840c50478cd9df5e
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-17 20:29:23 +0700

    Update for db schema file

--- ShipperAroundTheWorld/db/schema.rb
+++ ShipperAroundTheWorld/db/schema.rb
@@ -10,15 +10,87 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115143723) do
+ActiveRecord::Schema.define(version: 20161117132628) do
+
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "users_id"
+    t.string   "id_card"
+    t.index ["users_id"], name: "index_admins_on_users_id", using: :btree
+  end
+
+  create_table "contracts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.date     "deadline"
+    t.string   "bank_account_a"
+    t.string   "bank_account_b"
+    t.integer  "state"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "reports_id"
+    t.integer  "users_id"
+    t.index ["reports_id"], name: "index_contracts_on_reports_id", using: :btree
+    t.index ["users_id"], name: "index_contracts_on_users_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "users_id"
+    t.integer  "requests_id"
+    t.index ["requests_id"], name: "index_messages_on_requests_id", using: :btree
+    t.index ["users_id"], name: "index_messages_on_users_id", using: :btree
+  end
+
+  create_table "product_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "price"
+    t.string   "origin"
+    t.float    "weight",           limit: 24
+    t.integer  "state"
+    t.string   "content"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "users_id"
+    t.integer  "product_types_id"
+    t.integer  "contracts_id"
+    t.index ["contracts_id"], name: "index_requests_on_contracts_id", using: :btree
+    t.index ["product_types_id"], name: "index_requests_on_product_types_id", using: :btree
+    t.index ["users_id"], name: "index_requests_on_users_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "password_digest"
+    t.string   "phonenumber"
+    t.float    "rank",            limit: 24
+    t.integer  "totalvote"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "admins", "users", column: "users_id"
+  add_foreign_key "contracts", "reports", column: "reports_id"
+  add_foreign_key "contracts", "users", column: "users_id"
+  add_foreign_key "messages", "requests", column: "requests_id"
+  add_foreign_key "messages", "users", column: "users_id"
+  add_foreign_key "requests", "contracts", column: "contracts_id"
+  add_foreign_key "requests", "product_types", column: "product_types_id"
+  add_foreign_key "requests", "users", column: "users_id"
 end
