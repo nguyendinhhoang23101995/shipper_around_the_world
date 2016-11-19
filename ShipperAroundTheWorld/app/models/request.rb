6723530467fa7cede39e04173e41e3866096bb5e
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-19 15:19:24 +0700

    add validate maximum value for price

--- ShipperAroundTheWorld/app/models/request.rb
+++ ShipperAroundTheWorld/app/models/request.rb
@@ -2,7 +2,7 @@ class Request < ApplicationRecord
 	belongs_to :user
 	has_one :contract
 	validates :user_id, presence: true
-	validates_numericality_of :price, presence: true, :greater_than => 0
+	validates_numericality_of :price, presence: true, :greater_than => 0, :less_than => 50000
 	validates :content, length: {minimum: 6}
 	default_scope -> { order(created_at: :desc) }
 end
