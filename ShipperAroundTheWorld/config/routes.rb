49b2cb4faafea719f43b4bd1fcd6bce3e6105db7
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-20 23:15:47 +0700

    Add route /find

--- ShipperAroundTheWorld/config/routes.rb
+++ ShipperAroundTheWorld/config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
 	get    '/login',   to: 'sessions#new'
   	post   '/login',   to: 'sessions#create'
   	delete '/logout',  to: 'sessions#destroy'
-	
 	get  '/signup',    to: 'users#new'
 	post '/signup',	   to: 'users#create'
-	
-	get '/find', to: 'requests#find'
+	get '/find', 	   to: 'requests#find'
 	resources :users
 	resources :messages,		  only: [:create, :destroy]
 	resources :contracts
