be1020196e2c49b8f32fbafe44143fbb9ddd264f
Tran Linh <tranlinh265@gmail.com>
2016-11-20 21:21:25 +0700

    add function find

--- ShipperAroundTheWorld/app/controllers/requests_controller.rb
+++ ShipperAroundTheWorld/app/controllers/requests_controller.rb
@@ -6,26 +6,37 @@ class RequestsController < ApplicationController
 	end
 	def create
 		@request = current_user.requests.build(request_params)
-
-		if @request.save
-			flash[:success] = "Request created!"
-			redirect_to user_path(current_user)
-		else
-			flash[:danger] = @request.errors.full_messages.to_sentence
-			redirect_to user_path(current_user)
+		if @request.state == 0
+			if @request.save
+				flash[:success] = "Request created!"
+				redirect_to user_path(current_user)
+			else
+				flash[:danger] = @request.errors.full_messages.to_sentence
+				redirect_to user_path(current_user)
+			end
+		else 
+			$requests = Request.find_by_sql("select id from requests where origin = '#{@request.origin}' and product_type_id = #{@request.product_type_id};")
+			redirect_to :action => :find
 		end
 	end
 
 	def destroy
-		@request.destroy
-	    flash[:success] = "Request deleted"
-	    redirect_to request.referrer || root_url
+		if @request.state != 0
+			@request.destroy
+		    flash[:success] = "Request deleted"
+		    redirect_to request.referrer || root_url
+		end
 	end
+
+	def find
+		@request= Request.new
+	end
+
 
 	private
 
     def request_params
-		params.require(:request).permit(:content, :price, :origin, :product_type_id)
+		params.require(:request).permit(:content, :price, :origin, :product_type_id,:state)
     end
 
     def correct_user
