c4574e380f32ffe090c406a85c65b48ed2e3841f
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-20 15:00:24 +0700

    Convention code and add show method in cotracts controller

--- ShipperAroundTheWorld/app/controllers/requests_controller.rb
+++ ShipperAroundTheWorld/app/controllers/requests_controller.rb
@@ -1,9 +1,10 @@
 class RequestsController < ApplicationController
 	before_action :correct_user,   only: :destroy
+	
 	def new 
 		@request = Request.new
-		
 	end
+	
 	def create
 		@request = current_user.requests.build(request_params)
 
@@ -18,18 +19,18 @@ class RequestsController < ApplicationController
 
 	def destroy
 		@request.destroy
-	    flash[:success] = "Request deleted"
-	    redirect_to request.referrer || root_url
+		flash[:success] = "Request deleted"
+		redirect_to request.referrer || root_url
 	end
 
 	private
 
-    def request_params
-		params.require(:request).permit(:content, :price, :origin, :product_type_id)
-    end
+		def request_params
+			params.require(:request).permit(:content, :price, :origin_id, :product_type_id)
+		end
 
-    def correct_user
-      @request = current_user.requests.find_by(id: params[:id])
-      redirect_to root_url if @request.nil?
-    end
+		def correct_user
+			@request = current_user.requests.find_by(id: params[:id])
+			redirect_to root_url if @request.nil?
+		end
 end
