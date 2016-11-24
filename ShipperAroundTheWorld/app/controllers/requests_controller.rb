90cb0374200e02f9ca42b4866ddd33919266f771
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-24 09:52:54 +0700

    Update request controller for list all requests

--- ShipperAroundTheWorld/app/controllers/requests_controller.rb
+++ ShipperAroundTheWorld/app/controllers/requests_controller.rb
@@ -15,9 +15,24 @@ class RequestsController < ApplicationController
 				flash[:danger] = @request.errors.full_messages.to_sentence
 				redirect_to user_path(current_user)
 			end
-		else 
-			$requests = Request.find_by_sql("select id from requests where origin_id = #{@request.origin_id} and product_type_id = #{@request.product_type_id};")
-			redirect_to :action => :find
+		elsif @request.state == 6
+			if @request.origin_id == 0 && @request.product_type_id != 0
+				$requests = Request.find_by_sql("select id from requests where 
+													product_type_id = #{@request.product_type_id} 
+													order by created_at DESC;")
+			elsif @request.product_type_id == 0 && @request.origin_id != 0
+				$requests = Request.find_by_sql("select id from requests where 
+													origin_id = #{@request.origin_id}
+													order by created_at DESC;")
+			elsif @request.product_type_id != 0 && @request.origin_id != 0
+				$requests = Request.find_by_sql("select id from requests where 
+											origin_id = #{@request.origin_id} 
+											and product_type_id = #{@request.product_type_id}
+											order by created_at DESC;")
+			else
+				$requests = Request.find_by_sql("select id from requests order by created_at DESC;")
+			end
+			redirect_to :action => :index
 		end
 	end
 
@@ -28,22 +43,22 @@ class RequestsController < ApplicationController
 		@messages.each do |mess|
 			mess.destroy
 		end
-		if @request.state != 0
+		if @request.state == 0
 			@request.destroy
 		    flash[:success] = "Request deleted"
 		    redirect_to request.referrer || root_url
 		end
 	end
 
-	def find
-		@request= Request.new
+	def index
+		@request = Request.new
+		@message = Message.new
 	end
 
 	private
 
-
 		def request_params
-			params.require(:request).permit(:content, :price, :origin_id, :product_type_id,:state)
+			params.require(:request).permit(:content, :price, :origin_id, :product_type_id, :state)
 		end
     
 		def correct_user
