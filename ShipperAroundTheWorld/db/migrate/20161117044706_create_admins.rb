72b13245d40f1f8b5c33e6ab635225fe4bfbedb0
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-24 09:51:18 +0700

    Remove admin model

--- ShipperAroundTheWorld/db/migrate/20161117044706_create_admins.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdmins < ActiveRecord::Migration[5.0]
-  def change
-    create_table :admins do |t|
-      t.string :id_card
-
-      t.timestamps
-    end
-  end
-end
--- ShipperAroundTheWorld/db/migrate/20161117044744_add_fk_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class AddFkToAdmin < ActiveRecord::Migration[5.0]
-	def change
-		add_reference :admins, :user, foreign_key: true
-	end
-end
