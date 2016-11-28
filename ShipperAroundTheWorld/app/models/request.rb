bb302f1ba845318d18e64af0e14e1b4e530e0456
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-11-28 22:02:19 +0700

    Change code for search engine of requests

--- ShipperAroundTheWorld/app/models/request.rb
+++ ShipperAroundTheWorld/app/models/request.rb
@@ -6,4 +6,19 @@ class Request < ApplicationRecord
 	validates :content, length: {minimum: 6}
 	default_scope -> { order(created_at: :desc) }
 	has_many :messages
+
+	def self.search origin_id, product_type_id
+		print origin_id
+		print product_type_id
+		if origin_id != '0' && product_type_id == '0'
+			Request.where("origin_id = ?", "#{origin_id}")
+		elsif origin_id == '0' && product_type_id != '0'
+			Request.where("product_type_id = ?", "#{product_type_id}")
+		elsif ((origin_id == '0' && product_type_id == '0') || (origin_id.nil? && product_type_id.nil?))
+			Request.all
+		else
+			Request.where("product_type_id = ? and origin_id = ?", 
+						  "#{product_type_id}", "#{origin_id}")
+		end
+	end
 end
