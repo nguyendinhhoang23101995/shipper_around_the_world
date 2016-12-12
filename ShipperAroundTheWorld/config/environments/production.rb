a6c5520635efb0fc94d4d71d55d60cec3f3aabeb
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-12-12 12:29:25 +0700

    Config for mailer

--- ShipperAroundTheWorld/config/environments/production.rb
+++ ShipperAroundTheWorld/config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
 		:address        => 'smtp.sendgrid.net',
 		:port           => '587',
 		:authentication => :plain,
-		:user_name      => 'tranlinh265',
-		:password       => 'tranmanh12',
+		:user_name      => 'nhom495itss',
+		:password       => 'Nhom495itss',
 		:domain         => 'heroku.com',
 		:enable_starttls_auto => true
 	}
